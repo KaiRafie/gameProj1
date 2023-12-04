6d638ab8882d723a76a7bbef224f7ed1acad25a8
Matthew Veroutis <6243511@edu.vaniercollege.qc.ca>
2023-12-04 10:05:17 -0500

    fix souund final

--- ThirdLevel.java
+++ ThirdLevel.java
@@ -20,7 +20,7 @@ public class ThirdLevel extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1300, 900, 1); 
 
-        gameMusic =  new  GreenfootSound("BONUSLEVEL.wav");
+        gameMusic =  new  GreenfootSound("bonusShort.wav");
         
         gameMusic.setVolume(50);
         
